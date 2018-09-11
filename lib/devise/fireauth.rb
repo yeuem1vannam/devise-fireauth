85a53ec26f1677543c49fba3f09d8a00d6dad50b
p.hoai.le <yeuem1vannam@users.noreply.github.com>
2018-09-11 22:52:09 +0900

    INITIALIZE

---
+++ lib/devise/fireauth.rb
@@ -0,0 +1,7 @@
+require "devise/fireauth/version"
+
+module Devise
+  module Fireauth
+    # Your code goes here...
+  end
+end
---
+++ lib/devise/fireauth/version.rb
@@ -0,0 +1,5 @@
+module Devise
+  module Fireauth
+    VERSION = "0.0.1"
+  end
+end
