13d917c087002aab44d687e4d444a25eb93f998e
p.hoai.le <yeuem1vannam@users.noreply.github.com>
2018-09-19 19:02:08 +0700

    chore: config the rspec

--- spec/devise/fireauth_spec.rb
+++ spec/devise/fireauth_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Devise::Fireauth do
   it "has a version number" do
     expect(Devise::Fireauth::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
