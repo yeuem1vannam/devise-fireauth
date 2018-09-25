cba77f1e39947e1e22c5c3595d74fc5cb694f16b
p.hoai.le <yeuem1vannam@users.noreply.github.com>
2018-09-25 18:50:20 +0900

    chore(dummy): scaffolding Post with required login

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   resources :users
+  resources :posts
 end
---
+++ spec/dummy/spec/factories/posts.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :post do
+    user { nil }
+    title { "MyString" }
+    content { "MyString" }
+  end
+end
